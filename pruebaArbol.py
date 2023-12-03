c579b92bda67b3d02734317f38d2b9957f0b9a58
MilagrosArg <margibaymolina@itba.edu.ar>
2023-12-03 17:08:47 -0300

    arbol

--- pruebaArbol.py
+++ pruebaArbol.py
@@ -1,35 +1,30 @@
 from Proyecto import *
 from FuncionesFinal import *
+from Nodo import *
 
-class Nodo:
-    def __init__(self, valor):
-        self.valor = valor
-        self.izquierda = None
-        self.derecha = None
+class ArbolBinario:
+    def __init__(self):
+        self.raiz = None
 
-def insertar(raiz, valor:Proyecto):
-    if raiz is None:
-        return Nodo(valor)
-    else:
-        if convertirfecha_datetime(valor.fecha_inicio) < raiz.convertirfecha_datetime(valor.fecha_inicio):
-            raiz.izquierda = insertar(raiz.izquierda, valor)
+    def insertar(self, valor):
+        self.raiz = self._insertar(self.raiz, valor)
+
+    def _insertar(self, raiz, valor):
+        if raiz is None:
+            return Nodo(valor)
         else:
-            raiz.derecha = insertar(raiz.derecha, valor)
-    return raiz
+            if convertirfecha_datetime(valor.fecha_inicio) < raiz.convertirfecha_datetime(valor.fecha_inicio):
+                raiz.izquierda = self._insertar(raiz.izquierda, valor)
+            else:
+                raiz.derecha = self._insertar(raiz.derecha, valor)
+        return raiz
 
-def recorrer_inorden(raiz):
-    if raiz:
-        recorrer_inorden(raiz.izquierda)
-        print(raiz.valor, end=' ')
-        recorrer_inorden(raiz.derecha)
+    def recorrer_inorden(self):
+        self._recorrer_inorden(self.raiz)
+        print()
 
-# Crear un árbol binario de ejemplo
-arbol = None
-datos = [5, 3, 7, 2, 4, 6, 8]
-
-for dato in datos:
-    arbol = insertar(arbol, dato)
-
-# Imprimir el recorrido inorden del árbol
-print("Recorrido Inorden:")
-recorrer_inorden(arbol)
+    def _recorrer_inorden(self, raiz):
+        if raiz:
+            self._recorrer_inorden(raiz.izquierda)
+            print(raiz.valor, end=' ')
+            self._recorrer_inorden(raiz.derecha)
