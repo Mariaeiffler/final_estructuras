fcbea626a9510bddef777559f31f6c35ae067b20
MilagrosArg <margibaymolina@itba.edu.ar>
2023-12-03 17:28:08 -0300

    arboles

--- Nodo.py
+++ Nodo.py
@@ -1,6 +1,6 @@
 from Proyecto import *
 class Nodo:
-    def __init__(self, valor:Proyecto):
+    def __init__(self, valor):
         self.valor = valor
         self.izquierda = None
         self.derecha = None
