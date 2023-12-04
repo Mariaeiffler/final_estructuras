7c5ae1b5a978dbb32cff1974b35322b60d7594ac
muranga03 <muranga@itba.edu.ar>
2023-12-04 16:27:17 -0300

    cambio mati2

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from FuncionesFinal import *
 from Arbol import *
 from Lectura_archivos import *
 from datetime import datetime as dt
+import numpy as np
+# import matplotlib.pyplot as plt
+import sys
+sys.setrecursionlimit(5000)
 
 class Trabajo():
-    def __init__(self):
-        self.moneda,self.tipo_proy,self.estado,self.disciplina,self.proyectos,self.arbol=diccionario_proy()
-
-    def entrar(self): 
-        obtener_pickle(self, 'abrir') 
+    def __init__(self,moneda,tipo_proy,estado,disciplina,proyectos,arbol):
+        self.moneda=moneda
+        self.tipo_proy=tipo_proy
+        self.estado=estado
+        self.disciplina=disciplina
+        self.proyectos=proyectos
+        self.arbol=arbol
+        
+    def entrar(self):  
         seguir = True
         pregunta=menuPPL()
         while seguir==True: 
             match pregunta:
                 case 1:
                     '''Mostrar la distribución de los proyectos por área de investigación y sus correspondientes sub áreas'''
+                    print(self.proyectos)
                     pregunta=menuPPL()
                 case 2:
                     '''Visualizar el porcentaje de participación de las mujeres versus la participación de los hombres en los diferentes proyectos'''
@@ -27,14 +36,15 @@ class Trabajo():
                     pregunta=menuPPL()
                 case 5:
                     '''Guardar y visualizar una lista de proyectos ordenados por la fecha de inicialización'''
+                    anios = anios_punto5()
+                    self.arbol.recorrer_en_orden(anios)
                     pregunta=menuPPL()
+                    
                 case 6:
                     '''Visualizar la relación entre el monto de financiamiento solicitado y el monto de financiamiento otorgado'''
                     pregunta=menuPPL()
                 case 7:
                     seguir=False   
-                           
-        obtener_pickle(self, 'cerrar') 
         print('Se ha cerrado el programa con éxito.')
 
     def porcentaje_hombres_mujeres(self):
@@ -96,3 +106,37 @@ class Trabajo():
 if __name__ == "__main__":
     trabajo=Trabajo()
     trabajo.visualizar_tiempo_promedio()
+def obtener_pickle(): #cambiar cuando sepamos 
+    try:
+        with open ('final.pickle','rb') as fpickle:
+            trabajo = pickle.load(fpickle)
+        #agregar las cosas a cargar
+
+    except FileNotFoundError:
+        moneda,tipo_proy,estado,disciplina,proyectos,arbol=diccionario_proy()
+        trabajo=Trabajo(moneda,tipo_proy,estado,disciplina,proyectos,arbol)
+        with open ('final.pickle','wb') as fpickle:
+            pickle.dump(trabajo,fpickle)
+    return trabajo
+
+    
+        
+# def piechartpunto2(trabajo):
+#     mujeres, hombres = trabajo.porcentaje_hombres_mujeres()
+#     porcentajes = np.array([hombres,mujeres])
+#     labels = ["Hombres", "Mujeres"]
+#     explode = [0.1,0]
+#     colores = ['b','r']
+#     titulo = 'Porcentajes de hombres y mujeres involucrados en proyectos'
+#     plt.pie(porcentajes, explode = explode, startangle = 90, shadow = True, colors = colores)
+#     plt.legend(labels)
+#     plt.title(titulo, loc = 'center', pad = 20)
+#     print('El porcentaje de participacion de mujeres es: ', mujeres, '%')
+#     print ('El porcentaje de participacion de hombres es: ', hombres, '%')
+#     plt.show() 
+
+
+if __name__ == "__main__":
+    obtener_pickle().entrar()
+
+
